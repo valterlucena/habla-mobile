2141114b0c2ed42a82d38a311a8aeca5b06d8dac
Lucas Silva <lucashenrique125@gmail.com>
2018-11-12 22:40:18 -0300

    Removed login with credentials and added login with facebook

--- src/screens/login/login.tsx
+++ src/screens/login/login.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { View, Text, TextInput, TouchableOpacity, ActivityIndicator, StyleSheet } from "react-native";
 import KeyboardSpacer from 'react-native-keyboard-spacer';
 import firebase from 'firebase';
+import Expo from "expo";
+import { FontAwesome } from '@expo/vector-icons';
 
 export default class LoginScreen extends React.Component<{}, LoginState> {
     static navigationOptions = {
@@ -18,7 +20,7 @@ export default class LoginScreen extends React.Component<{}, LoginState> {
         return (
             <View style={styles.page.container}>
                 <Text style={styles.login.headerText}>Habla!</Text>
-                <TextInput placeholder="Email"
+                {/* <TextInput placeholder="Email"
                            style={styles.login.input}
                            editable={!this.state.loading}
                            underlineColorAndroid='rgba(0,0,0,0)'
@@ -30,20 +32,31 @@ export default class LoginScreen extends React.Component<{}, LoginState> {
                            secureTextEntry={true}
                            onChangeText={text => this.setState({ credentials: { ...this.state.credentials, password: text }})}></TextInput>
                 <TouchableOpacity style={styles.login.loginButton}
-                                  onPress={this.login}
+                                  onPress={this.signInWithEmailAndPassword}
                                   disabled={this.state.loading}
                                   activeOpacity={1}>
                     {this.state.loading? 
                           (<ActivityIndicator color="white"
                                               size="small"/>)
                         : (<Text style={styles.login.loginButtonText}>Sign in</Text>) }
+                </TouchableOpacity> */}
+                <TouchableOpacity style={styles.login.loginButtonFacebook}
+                                  onPress={this.signInWithFacebook}
+                                  disabled={this.state.loading}
+                                  activeOpacity={1}>
+                    {this.state.loading? 
+                          (<ActivityIndicator color="white"
+                                              size="small"/>)
+                        : (<View style={styles.login.loginButtonInnerView}>
+                            <FontAwesome name="facebook" style={styles.login.facebookIcon}/><Text style={styles.login.loginButtonText}>Sign in with Facebook</Text>
+                           </View>) }
                 </TouchableOpacity>
                 <KeyboardSpacer/>
             </View>
         )
     }
 
-    login = async() => {
+    signInWithEmailAndPassword = async() => {
         this.setState({ loading: true });
 
         try {
@@ -53,6 +66,21 @@ export default class LoginScreen extends React.Component<{}, LoginState> {
           console.log(error);
         }
     };
+
+    signInWithFacebook = async() => {
+        this.setState({ loading: true });
+
+    
+        try {
+          const result = await Expo.Facebook.logInWithReadPermissionsAsync("2136539466408117", { behavior: "web"});
+          const credential = firebase.auth.FacebookAuthProvider.credential(result.token);
+          
+          await firebase.auth().signInAndRetrieveDataWithCredential(credential);
+        } catch (error) {
+          this.setState({ loading: false });
+          console.log(error);
+        }
+    }
 }
 
 interface LoginState {
@@ -84,18 +112,27 @@ const styles = {
             marginBottom: 10,
             fontSize: 18,
         },
-        loginButton: {
+        loginButtonFacebook: {
             paddingHorizontal: 14,
             paddingVertical: 14,
-            backgroundColor: "#795548",
+            backgroundColor: "#4267b2",
             width: '100%',
-            borderRadius: 5
+            borderRadius: 5,
+            alignItems: "center"
+        },
+        facebookIcon: {
+            color: "#ffffff",
+            fontSize: 18,
+            marginRight: 10
         },
         loginButtonText: {
             fontSize: 18,
             textAlign: 'center',
             color: "#FFFFFF",
             fontWeight: "bold"
+        },
+        loginButtonInnerView: {
+            flexDirection: "row"
         }
     })
 }
