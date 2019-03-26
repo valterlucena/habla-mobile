4254a12060445428ab88cbc5590044867e6be795
Valter Lucena <valter.lucena@ccc.ufcg.edu.br>
2019-03-26 20:28:01 -0300

    cria estrutura da tela de edição de perfil

---
+++ src/screens/profile-edition/profile-edition.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Text, View, SafeAreaView } from "react-native";
+import { client } from "../../services/client";
+import gql from "graphql-tag";
+import THEME from "../../theme/theme";
+import i18n from 'i18n-js';
+
+export default class ProfileCreationScreen extends React.Component<any, any> {
+  constructor(props: any){
+      super(props);
+
+      this.state = {};
+  }
+
+  render() {
+    return (
+      <SafeAreaView>
+          <View>
+          </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = {}
