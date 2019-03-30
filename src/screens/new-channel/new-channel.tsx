11f758861f113f9caeb65b3fbae0842d0861de33
alicesilva <alice.silva@ccc.ufcg.edu.br>
2019-03-30 19:10:39 -0300

    criar canal com backend

--- src/screens/new-channel/new-channel.tsx
+++ src/screens/new-channel/new-channel.tsx
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View, StatusBar, TouchableOpacity, TextInput, Activit
 import { FontAwesome } from '@expo/vector-icons';
 import THEME from '../../theme/theme';
 import i18n from 'i18n-js';
+import { client } from '../../services/client';
+import gql from 'graphql-tag';
 
 export default class NewChannelScreen extends React.Component<NewChannelScreenProps, NewChannelScreenState>{
     constructor(props: NewChannelScreenProps) {
@@ -11,11 +13,53 @@ export default class NewChannelScreen extends React.Component<NewChannelScreenPr
         this.state = { channel: { body: null }, teste: false };
     }
 
+    componentWillMount() {
+        this.resetChannel();
+    }
+
     dismiss = () => {
         this.props.onDismiss && this.props.onDismiss();
     }
 
+    handleChannelInput = (text: string) => {
+        this.setState({ channel: { ...this.state.channel, body: text }});
+      }
+
     sendChannel = async() => {
+        this.setState({ teste: true });
+    
+        try {
+          const response = await client.mutate({
+            variables: { 
+              channel: this.state.channel
+            },
+            mutation: gql(`
+              mutation CreateChannel ($channel: ChannelInput!) {
+                createChannel(channel: $channel) {
+                  id,
+                  name,
+                  createdAt
+                }
+              }
+            `),
+            context: {
+            }
+          });
+    
+          this.props.onSuccess && this.props.onSuccess(response.data.createChannel);
+        } catch (error) {
+          console.log(error);
+        } 
+    
+        this.setState({  teste: false });
+    }
+
+      resetChannel() {
+        this.setState({
+          channel: {
+            
+          }
+        });
       }
 
     render() {
@@ -28,12 +72,16 @@ export default class NewChannelScreen extends React.Component<NewChannelScreenPr
                     </TouchableOpacity>
                 </View>
                 <TextInput style={styles.newChannel.input}
-                  multiline={true}
+                  onChangeText={this.handleChannelInput}
+                  value={this.state.channel.body}
                   placeholderTextColor="white"
-                  placeholder={i18n.t('screens.newChannel.inputPlaceholder')}
+                  placeholder={i18n.t('screens.newPost.inputPlaceholder')}
+                  editable={!this.state.teste}
+                  multiline={true}
                   underlineColorAndroid="rgba(0,0,0,0)"/>
                 <TouchableOpacity style={styles.newChannel.sendButton}
                         onPress={this.sendChannel}
+                        disabled={this.state.teste || !this.state.channel.body || this.state.channel.body.trim() == ''}
                           activeOpacity={1}>
                      {this.state.teste? <ActivityIndicator color="white"/>: <Text style={styles.newChannel.sendButtonText}>{ i18n.t('screens.newPost.buttons.submit') }</Text>}
                 </TouchableOpacity>
