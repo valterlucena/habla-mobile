c8c046ccadbffdc08f1acbcc9ad5e4aba65574cd
Lucas Silva <lucashenrique125@gmail.com>
2019-05-07 23:08:51 -0300

    Fix user avatar

--- src/components/post/post.tsx
+++ src/components/post/post.tsx
@@ -82,7 +82,7 @@ export default class PostComponent extends React.Component<PostComponentProps, P
         (<View style={styles.header}>
           <TouchableOpacity style={styles.avatar} disabled={ !this.state.post.owner } onPress={() => this.props.onOpenProfile && this.props.onOpenProfile(this.state.post.owner)}>
             { this.state.post.owner && this.state.post.owner.photoURL?  
-            <Image style={styles.avatarIconImage as any} source={this.state.post.owner.photoURL? { uri: this.state.post.owner.photoURL }: photoDefault} width={40} height={40}/>:
+            <Image style={styles.avatarIconImage as any} source={{ uri: this.state.post.owner.photoURL }} width={40} height={40}/>:
             <FontAwesome style={styles.avatarIcon} name="user-circle"/>}
             {this.state.post.anonymous? <Text style={styles.headerText}>{ i18n.t('global.user.anonymousLabel')}</Text>: <Text style={styles.headerText}>{ this.state.post.owner.username }</Text>}
           </TouchableOpacity>
